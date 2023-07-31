dd54ebf1515b5e87c4fc6a719b28458ba036c06f
khanh-devos <dreamproperty.khanh@gmail.com>
2023-07-31 21:15:31 +0700

    add My profile

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 import Missions from './components/missions/Mission';
 import Rockets from './components/rockets/Rocket';
 import Logo from './planet.png';
+import MyProfile from './components/MyProfile';
 
 function App() {
   const colorActiveRoute = ({ isActive }) => (isActive ? { textDecoration: 'underline' } : { textDecoration: 'none' });
@@ -24,12 +25,15 @@ function App() {
           <navigator>
             <NavLink style={colorActiveRoute} to="/">Rockets</NavLink>
             <NavLink style={colorActiveRoute} to="/mission">Missions</NavLink>
+            <NavLink style={colorActiveRoute} to="/myprofile">My Profile</NavLink>
+
           </navigator>
         </header>
 
         <Routes>
           <Route path="/" element={<Rockets />} />
           <Route path="/mission" element={<Missions />} />
+          <Route path="/myprofile" element={<MyProfile />} />
 
         </Routes>
       </BrowserRouter>
